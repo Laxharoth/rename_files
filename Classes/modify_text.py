cddd788d2153086f84bbc2207b4f52c6e7ad0485
Luis Daniel Espino Mandujano <zeydenlaxharoth@gmail.com>
2024-10-09 11:29:28 -0600

    Create modify_text.py

---
+++ Classes/modify_text.py
@@ -0,0 +1,98 @@
+from typing import Protocol
+import re
+
+
+class TextGenerator(Protocol):
+    def __call__(self, input:str)->str:
+        ...
+
+generator_identity:TextGenerator = lambda x: x 
+
+class generator_counter:
+    def __init__(self, initial, increase):
+        self.increase = increase
+        self.value:int = initial-increase
+    
+    def __call__(self, _x:str)->int:
+        self.value += self.increase
+        return self.value
+    
+# Function that finds and creates TextGenerators for each wildcard. returns list of TextGenerators and search strings
+def get_text_generators(pattern:str) -> tuple[[list[str], list[TextGenerator]]]:
+    """Creates a list of required text patterns and TextGenerators\n    
+    '*' means wildcard \n
+        if pattern is\n
+            abc * efg\n
+        and text is \n
+            abc lorem efg\n
+        the pattern will match and the wildcard will be lorem\n
+    '{d:d}' means counter where the first 'd' is the initial value and the second 'd' is the increment (d can only be integers >=0)\n
+        if pattern is \n
+            abc {1:3} efg\n
+        and text is \n
+            abc lorem efg\n
+        the pattern will match and the wildcard will be 1 and increment to '{4,3}'\n
+    Args:
+        pattern (str): text representing pattern to match
+
+    Returns:
+        _type_: tuple with two list the first is the values expected to be in a string that is going to be modified, the second is a list of the TextGenerators
+    """
+    search_regex = "(°/0)|({\\d+:\\d+})"
+    pattern = pattern.replace("*", "°/0")
+    iter = re.finditer(search_regex, pattern)
+    end = [m.start(0) for m in iter] + [len(pattern)]
+    iter = re.finditer(search_regex, pattern)
+    start = [0] + [m.end(0) for m in iter]
+    # get static patterns
+    static_patterns = [pattern[a:b] for a,b in zip(start,end)]
+    
+    wildcards = re.findall(search_regex, pattern)
+    generators:list[TextGenerator] = []
+    
+    for wildcard in wildcards:
+        if re.match("°/0",wildcard[0]):
+            generators.append(generator_identity)
+        if re.match("{\\d+:\\d+}",wildcard[1]):
+            generators.append(generator_counter(*map(int,wildcard[1][1:-1].split(":"))))
+    
+    return static_patterns ,generators
+
+def elements_in_order(s: str, lst: list) -> bool:
+    start_idx = 0    
+    for elem in lst:
+        found_idx = s.find(elem, start_idx)
+        if found_idx == -1:
+            return False
+        start_idx = found_idx + len(elem)
+    
+    return True
+
+# Function that creates the text given a string patterns
+def generate_text(original:str, target:str, statics:list[str], generators:list[TextGenerator]):
+    """replaces the text of original with target using generators
+
+    Args:
+        original (str): _description_
+        target (str): _description_
+        statics (list[str]): _description_
+        generators (list[TextGenerator]): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if not elements_in_order(original, statics):
+        return original
+    starting_index = 0
+    limits = zip(statics[:-1], statics[1:])
+    
+
+if __name__ == "__main__":
+    pattern = u"*¿abc asdf * qwerwe * zcvzxv* {1:2} ñlkñl"
+    example_original = u"lorem abc asdf ipsum qwerwe yuyun zcvzxvgolian gamma ñlkñl"
+    example_target = u"yunaml"
+    statics, generators = get_text_generators(pattern)
+    # Test for get_text_generators
+    
+
+    
